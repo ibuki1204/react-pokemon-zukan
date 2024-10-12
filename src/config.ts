a11e980a38a534e273824015a23da96e97c124a5
ibuki1204 <KTC24A32J0003@edu.kyoto-tech.ac.jp>
2024-10-12 11:45:33 +0900

    14

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL='https://pokeapi.co/api/v2'
+const API_BASE_URL='https://pokeapi.co/api/v2'
 const INITIAL_POKEMON_LIST_LIMIT = 40;
 export { API_BASE_URL, INITIAL_POKEMON_LIST_LIMIT };
