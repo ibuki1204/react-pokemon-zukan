7480d92ec15481c3ee17cd1e66a401c3d3412d18
ibuki1204 <ktc24a32j0003@edu.kyoto-tech.ac.jp>
2024-10-12 10:36:29 +0900

    commit

--- react-pokemon-zukan/src/main.tsx
+++ react-pokemon-zukan/src/main.tsx
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
 import './index.css';
@@ -29,3 +16,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     </QueryClientProvider>
   </React.StrictMode>
 );
+
